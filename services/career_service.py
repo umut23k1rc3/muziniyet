89586931615174bd6cb62de3525c7246252d03e2
umut23k1rc3 <umutkirci45@gmail.com>
2026-01-22 23:08:47 +0300

    Update career_service.py

--- services/career_service.py
+++ services/career_service.py
@@ -5,8 +5,20 @@ class CareerService:
         with open("data/careers.json", encoding="utf-8") as f:
             self.data = json.load(f)
 
-    def get_careers(self, field):
-        return self.data[field]["careers"]
+    def get_fields(self):
+        return self.data.keys()
 
-    def get_title(self, field):
-        return self.data[field]["title"]
+    def get_field(self, field):
+        return self.data[field]
+
+    def format_careers(self, field, level):
+        field_data = self.data[field]
+        result = f"**{field_data['title']}**\n\n"
+
+        for career in field_data["careers"]:
+            result += f"### {career['name']}\n"
+            result += f"**Skills:** {', '.join(career['skills'])}\n"
+            result += f"**Education:** {career['education']}\n"
+            result += f"**Salary ({level.title()}):** {career['salary'][level]}\n\n"
+
+        return result
