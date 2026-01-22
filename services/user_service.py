cb7376be88708cb98f454584b293540b6dced67d
umut23k1rc3 <umutkirci45@gmail.com>
2026-01-22 23:28:15 +0300

    Delete services directory

--- services/user_service.py
+++
@@ -1,9 +0,0 @@
-class UserService:
-    def __init__(self):
-        self.users = {}
-
-    def set_interest(self, user_id, field):
-        self.users[user_id] = field
-
-    def get_interest(self, user_id):
-        return self.users.get(user_id)
